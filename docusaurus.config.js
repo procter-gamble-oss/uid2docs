f5edf99017a56e5d32c46f4e504262e09bd017a7
bbuilds <hello@brandenbuilds.com>
2023-07-26 12:25:04 -0500

    update docu config with staging settings

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,15 +20,15 @@ const config = {
   favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: "https://unifiedid.com",
+  url: "https://unifiedid2.github.io",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: "IABTechLab", // Usually your GitHub org/user name.
-  projectName: "uid2docs", // Usually your repo name.
+  organizationName: "UnifiedID2", // Usually your GitHub org/user name.
+  projectName: "uid2-docs-preview", // Usually your repo name.
   trailingSlash: false,
 
   onBrokenLinks: "warn",

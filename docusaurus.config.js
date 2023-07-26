4f7a3684a353f05517fb9543965232f8ef130924
bbuilds <hello@brandenbuilds.com>
2023-07-26 12:31:43 -0500

    update base URL

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -23,7 +23,7 @@ const config = {
   url: "https://unifiedid2.github.io",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/",
+  baseUrl: "/uid2-docs-preview/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
